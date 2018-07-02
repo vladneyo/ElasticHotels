9ecd05c7666546e9a29a16be07c167396b58171a
KYIV\vlne <vlne@ciklum.com>
2018-07-02 18:56:33 +0300

    Initial structure

---
+++ ElasticHotels.Data/Constants/GoogleConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticHotels.Data.Constants
+{
+    public class GoogleConstants
+    {
+        public const string GooglePlacesAPIKey = "AIzaSyAi9Yeb6X-twTIlouz8_y66DQ5GBBoav_w";
+    }
+}
---
+++ ElasticHotels.Data/Constants/PlaceTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticParties.Data.Constants
+{
+    public class PlaceTypes
+    {
+        public const string AmusementPark = "amusement_park";
+        public const string Bar = "bar";
+        public const string Cafe = "cafe";
+        public const string Casino = "casino";
+        public const string LiquorStore = "liquor_store";
+        public const string NightClub = "night_club";
+    }
+}
