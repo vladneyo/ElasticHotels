320459fb2c87640b0fdb65bfbe2d281f300ca792
KYIV\vlne <vlne@ciklum.com>
2018-07-30 17:19:59 +0300

    Schema consts for doc was added

---
+++ ElasticParties.Lucene.Data/Constants/Schema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticParties.Lucene.Data.Constants
+{
+    public class Schema
+    {
+        public static readonly string Id = "Id";
+        public static readonly string Name = "Name";
+        public static readonly string PlaceId = "PlaceId";
+        public static readonly string Rating = "Rating";
+        public static readonly string Vicinity = "Vicinity";
+        public static readonly string Types = "Types";
+        public static readonly string OpenNow = "OpeningHours.OpenNow";
+        public static readonly string Location = "Geometry.Location";
+    }
+}
